33da337ffa1a56e23fccd93e0baa0303a5f321bb
aymen-ameri <ameri.aymen@esprit.tn>
2023-04-13 02:05:09 +0200

    tp1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
+import { ArticlesComponentComponent } from './articles-component/articles-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OffresEmploiComponentComponent,
+    ArticlesComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
